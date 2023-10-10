0dc5cbe8dfd80b261be6102442f5d3b3fa9667d1
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-10-10 11:17:43 +0100

    a pinia todo

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <main>
+      <header>
+        <img src="./assets/pinia-logo.svg" alt="Pinia Logo" />
+        <h1>Pinia Task</h1>
+      </header>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
